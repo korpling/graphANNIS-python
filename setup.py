1ceb75f7a2a011196f17c959c2926615acf66ebd
Thomas Krause <thomaskrause@posteo.de>
2019-04-14 15:08:28 +0200

    Set version to 0.19.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README_pypi.md') as f:
     long_description = f.read()
 
 setup(name='graphannis',
-      version='0.19.1',
+      version='0.19.2',
       description='graphANNIS Python API',
       author='Thomas Krause',
       author_email='thomaskrause@posteo.de',
