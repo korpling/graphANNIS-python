87afb3975b7d9399ea58ab01f5dd0f2f76f181fb
Thomas Krause <thomaskrause@posteo.de>
2019-08-14 15:22:28 +0200

    Add custom (still empty) command to download the released binaries to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup, find_packages, Command
+
+# Package meta-data
+VERSION = '0.22.0'
+CORE_VERSION = '0.22.0'  # graphANNIS core library version
+
+
+class CoreLibraryCommand(Command):
+    """Download released graphANNIS core library binaries"""
+
+    description = 'Download released graphANNIS core library binaries'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status('Downloading released graphANNIS ' +
+                    VERSION + ' core library binaries')
+
+        sys.exit()
+
 
 with open('README_pypi.md') as f:
     long_description = f.read()
 
 setup(name='graphannis',
-      version='0.22.0',
+      version=VERSION,
       description='graphANNIS Python API',
       author='Thomas Krause',
       author_email='thomaskrause@posteo.de',
@@ -17,12 +47,15 @@ setup(name='graphannis',
       include_package_data=True,
       setup_requires=["cffi>=1.0.0"],
       cffi_modules=["package/graphannis_build.py:ffibuilder"],
-      install_requires=["cffi>=1.0.0","networkx"],
+      install_requires=["cffi>=1.0.0", "networkx"],
       classifiers=(
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows"
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: POSIX :: Linux",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows"
       ),
-     )
+      cmdclass={
+          'build_clib': CoreLibraryCommand
+      }
+      )
