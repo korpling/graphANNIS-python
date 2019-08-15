1c4b09f64039e1169287dbdfed999a078e496758
Thomas Krause <thomaskrause@posteo.de>
2019-08-15 11:25:39 +0200

    Output status when preparing the release

--- release_prepare.sh
+++ release_prepare.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Building package and downloading release binaries"
 python3 setup.py clean
 python3 setup.py build
+echo "Running unit tests"
 python3 -m unittest
+echo "Running documentation tests"
 ./doctest_runner.py
+echo "Updating changelog header"
 python3 ./package/update_changelog.py
